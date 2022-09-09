62b5acae76ff1e5d5a8d520aa989ef0b0bf3b32f
Aldo Aníbal Colombo <44879977+acolombo1@users.noreply.github.com>
2022-09-09 20:46:59 -0300

    Fix password change and confirmation resend problems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,20 +36,14 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  config.action_mailer.perform_caching = false
+  # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'myspendings.acolombo1@gmail.com',
-    password:             'ejehwxejnzglwmct',
-    authentication:       'plain',
-    enable_starttls_auto: true
-  }
+
+  config.action_mailer.perform_caching = false
+  
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :letter_opener
   
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
